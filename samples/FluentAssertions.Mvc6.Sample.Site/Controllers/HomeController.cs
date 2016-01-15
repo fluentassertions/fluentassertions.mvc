fa8090cd9deffd5cc1d5679b9ef037f0d59c1838
Kevin Kuszyk <kevin@kevinkuszyk.com>
2016-01-15 16:24:43 +0000

    Add MVC6 project and sample site

---
+++ samples/FluentAssertions.Mvc6.Sample.Site/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace FluentAssertions.Mvc6.Sample.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = "foo";
+            return View(model);
+        }
+    }
+}
---
+++ src/FluentAssertions.Mvc6/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FluentAssertions.Mvc6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FluentAssertions.Mvc6")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("85f5b2a6-49cd-43b1-a75a-5e6dd87de399")]
